1d5787b370107f838e6b3d927a15b4fc4a09bb1b
Pascal DENIS <pascal.denis.75@gmail.com>
2015-04-02 22:10:06 +0200

    styles

--- server/method.js
+++ server/method.js
@@ -1,25 +1,33 @@
 if (Meteor.isServer) {
     Meteor.methods({
         fetchFromService: function(owner) {
-            var url = "https://api.travis-ci.org/repos.json?owner_name="+owner;
+            var travis_url = "https://api.travis-ci.org/repos.json?owner_name="+owner;
+            var scrutinizer_url = "https://scrutinizer-ci.com/api/repositories/g/";
             //synchronous GET
-            var result = Meteor.http.get(url, {timeout:30000});
+            var result = Meteor.http.get(travis_url, {timeout:30000});
             if(result.statusCode==200) {
                 console.log("response received.");
+                Repos.remove({});
                 JSON.parse(result.content).forEach(function(repo){
-                    repo = {
-                        slug: repo.slug,
-                        build_status: repo.last_build_status,
-                        build_date: repo.last_build_finished_at
-                    };
+                    if (repo.last_build_status != null) {
+                        try {
+                            scrutinizerRepo = Meteor.http.get(scrutinizer_url + repo.slug, {timeout:30000}).content;
 
+                            console.log(scrutinizerRepo);
+                            repo = {
+                                slug: repo.slug,
+                                build_status: repo.last_build_status,
+                                build_date: repo.last_build_finished_at
+                            };
 
-                    doc = Repos.findOne({slug: repo.slug});
-                    if (doc) {
-                        console.log(repo);
-                        Repos.update(doc._id, {$set: repo});
-                    } else {
-                        Repos.insert(repo);
+                            doc = Repos.findOne({slug: repo.slug});
+                            if (doc) {
+                                console.log(repo);
+                                Repos.update(doc._id, {$set: repo});
+                            } else {
+                                Repos.insert(repo);
+                            }
+                        } catch(err) {}
                     }
                 });
             } else {
